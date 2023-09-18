6f317709006b6102636915888f3a0a50a19be087
jakubfiglak <jakub.figlak@netguru.com>
2023-09-18 09:46:24 +0200

    feat: shadcn for basic styling

--- app/xstate/promise-machine/content.tsx
+++ app/xstate/promise-machine/content.tsx
@@ -2,34 +2,23 @@
 
 import { useMachine } from "@xstate/react";
 import { promiseMachine } from "./machine";
+import { Button } from "@/components/ui/button";
 
 export const Content = () => {
   const [state, send] = useMachine(promiseMachine);
 
   return (
-    <div>
+    <div className="text-center space-y-4">
       {/** You can listen to what state the service is in */}
       {state.matches("pending") && <p>Loading...</p>}
       {state.matches("rejected") && <p>Promise Rejected</p>}
       {state.matches("resolved") && <p>Promise Resolved</p>}
-      <div>
+      <div className="flex items-center gap-4">
         {/** You can send events to the running service */}
-        <button
-          onClick={() => {
-            console.log("RESOLVING");
-            return send("RESOLVE");
-          }}
-        >
-          Resolve
-        </button>
-        <button
-          onClick={() => {
-            console.log("REJECTING");
-            return send("REJECT");
-          }}
-        >
+        <Button onClick={() => send("RESOLVE")}>Resolve</Button>
+        <Button onClick={() => send("REJECT")} variant="destructive">
           Reject
-        </button>
+        </Button>
       </div>
     </div>
   );
